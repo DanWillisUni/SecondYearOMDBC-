ebdad674ac487e6ae67330895b867fbd1040a73b
DanWillisUni <dan.willis.uni@gmail.com>
2020-04-06 14:29:39 +0100

    relative file path

--- main.cpp
+++ main.cpp
@@ -3,14 +3,7 @@
 #include "MovieDatabase.h"
 
 int main() {
-//    Movie ma("Indiana Jones and the Last Crusade", 1989, "PG-13", "Action/Adventure/Fantasy", 127, 0);
-//    ma.print();
-//    Movie mb("'The Great Dictator',1940,'APPROVED','Comedy/Drama/War',125,0");
-//    mb.print();
-//    MovieDatabase database;
-//    database.add(ma);
-//    database.add(mb);
-    MovieDatabase database = MovieDatabase("C:\\Users\\danny\\CLionProjects\\CourseWork2\\films.txt");
+    MovieDatabase database = MovieDatabase("../films.txt");
     cout << "Printing database:" << endl;
     database.print();
     return 0;
