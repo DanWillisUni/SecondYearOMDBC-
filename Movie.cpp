15dd868c4d4ae2c6c3ea3c0b2165fb07418bd964
DanWillisUni <dan.willis.uni@gmail.com>
2020-04-15 14:10:05 +0100

    more thourough comments on movie.cpp and movie.h

--- Movie.cpp
+++ Movie.cpp
@@ -4,6 +4,7 @@
 
 #include "Movie.h"
 using namespace std;
+
 const map<string, Movie::certificateEnum> Movie::certificateStringToEnum = {
         {"PG-13", PG_13},
         {"PG", PG},
@@ -18,9 +19,16 @@ const map<string, Movie::certificateEnum> Movie::certificateStringToEnum = {
         {"M",M},
         {"X",X}
 };
+/**
+ * Test harness
+ * Constructs 2 movies, a and b using two different constructors
+ * prints the 2 movies
+ * see if each movie has genre action
+ * see if each movie has the genre fantasy
+ */
 void Movie::tester(){
     Movie a = Movie("Indiana Jones and the Last Crusade",1989,"PG-13","Action/Adventure/Fantasy",127,0);
-    Movie b = Movie("\"Aliens\",1986,\"R\",\"Action/Adventure/Sci-Fi\",137,0");
+    Movie b = Movie(R"("Aliens",1986,"R","Action/Adventure/Sci-Fi",137,0)");
     cout << "a: ";
     cout << a;
     cout << "b: ";
@@ -31,14 +39,14 @@ void Movie::tester(){
     cout << "b has genre \"Fantasy\": " << b.hasGenre("Fantasy")<< "\n";
 }
 /**
- * Unused Constructor
+ * Constructor
  * Constructs a movie from all the attributes passed in
- * @param title title of the movie
- * @param releaseYear year or release of the movie
- * @param certificate certificate of the movie
- * @param genres genres of the movie
- * @param duration duration of the movie
- * @param averageRating average rating of the movie
+ * @param title, title of the movie
+ * @param releaseYear, year or release of the movie
+ * @param certificate, certificate of the movie
+ * @param genres, genres of the movie
+ * @param duration, duration of the movie
+ * @param averageRating, average rating of the movie
  */
 Movie::Movie(const string& title, const int& releaseYear, const string& certificate, const string& genres, const int& duration, const int& averageRating) {//Constructor
     m_title = title;
@@ -51,6 +59,7 @@ Movie::Movie(const string& title, const int& releaseYear, const string& certific
 /**
  * Constructor
  * Creates a movie from a line in a file
+ * has to be in the correct format
  * @param line the line of the file
  */
 Movie::Movie(const string& line){
@@ -84,14 +93,14 @@ int Movie::getAverageRating() const{
     return m_averageRating;
 }
 /**
- * to find if a movie is of a specified genre
+ * Finds if a movie is of a specified genre
  * using find function
  * @param genreToMatch desired genre
  * @return true if the movie is of the desired genre
  */
 bool Movie::hasGenre(const string& genreToMatch) const{
-    size_t found = getGenres().find(genreToMatch);
-    if (found!=string::npos){
+    size_t found = getGenres().find(genreToMatch);//gets the position of the start of the the string to find
+    if (found!=string::npos){//if the position isn't the end of the string it has been found and therefore the movie has the genre
         return true;
     }
     return false;
@@ -107,35 +116,35 @@ ostream& operator<< (ostream &out, const Movie &m) {
     return out; // return ostream so I can chain calls to operator<< used for the
 }
 /**
- * Splits the string to split on each separator string
+ * Splits the string to split on each separator string into a vector
  * @param str string to split
  * @param seperator string to split on
  * @return vector of all the substrings split
  */
 vector<string> Movie::splitString(const string& str, const string& seperator) {
     vector<string> parts;//parts of the string after split
-    size_t pos = 0;// start at the begining
+    size_t pos = 0;//start at the beginning
     while (true) {
-        size_t newPos = str.find(seperator, pos);//try to find the place of the next seperator
-        if (newPos == string::npos) {// No more seperators
-            parts.push_back(str.substr(pos));//pushback last substring
+        size_t newPos = str.find(seperator, pos);//try to find the place of the next separator
+        if (newPos == string::npos) {// No more separators
+            parts.push_back(str.substr(pos));//push back last substring
             break;//exit the while
         }
-        // Found seperator at newPos
+        // Found separator at newPos
         parts.push_back(str.substr(pos, newPos - pos));//push back new substring
-        pos = newPos + seperator.length();//jumps to the end of the seperator
+        pos = newPos + seperator.length();//jumps to the end of the separator
     }
     return parts;
 }
 /**
  * Search for the key string of the certificate from the certificate enum
- * @param cE certificate enum to match
+ * @param certificateEnum certificate enum to match
  * @return the string, matching the certificate enum
  */
-string Movie::searchForKey(const Movie::certificateEnum& cE){
+string Movie::searchForKey(const Movie::certificateEnum& certificateEnum){
     string key;
     for (auto &i : Movie::certificateStringToEnum) {//goes through the map
-        if (i.second == cE) {//compares the second item (the value) to
+        if (i.second == certificateEnum) {//compares the second item (the value) to
             key = i.first; //the key is set
             break; //To stop searching
         }
--- Movie.h
+++ Movie.h
@@ -10,9 +10,10 @@
 #ifndef COURSEWORK2_MOVIE_H
 #define COURSEWORK2_MOVIE_H
 using namespace std;
+
 class Movie{
 public:
-    enum certificateEnum {//enum of all the certificates that can happen
+    enum certificateEnum {//enum of all the certificates that are in the films.txt file
         PG_13,
         PG,
         APPROVED,
@@ -34,28 +35,28 @@ private:
     int m_duration;//duration of the film in minutes
     int m_averageRating;//average viewer rating
     static vector<string> splitString(const string& str, const string& seperator); //split string function
-    static string searchForKey(const certificateEnum& cE);
+    static string searchForKey(const certificateEnum& certificateEnum);
 public:
     static const map<string, certificateEnum> certificateStringToEnum;//map for converting the string to the enum type
     static void tester();//test harness
-    Movie(const string& title, const int& releaseYear, const string& certificate, const string& genres, const int& duration, const int& averageRating);
+    Movie(const string& title, const int& releaseYear, const string& certificate, const string& genres, const int& duration, const int& averageRating);//constructs from all the attributes
     explicit Movie(const string& line);//creates a movie from the line in the file
     Movie();//empty movie constructor
-    string getTitle() const;
-    int getReleaseYear() const;
-    certificateEnum getCertificate() const;
-    string getGenres() const;
-    int getDuration() const;
-    int getAverageRating() const;
-    bool hasGenre(const string& genreToMatch) const;
+    string getTitle() const; //gets the title of the movie
+    int getReleaseYear() const;//gets the release year of the movie
+    certificateEnum getCertificate() const;//gets the certificate of the movie
+    string getGenres() const;//gets the genre string of the movie
+    int getDuration() const;//gets the duration of the movie
+    int getAverageRating() const;//gets the average rating of the movie
+    bool hasGenre(const string& genreToMatch) const;//gets if the movie is of the specified genre
 
     friend ostream& operator<< (std::ostream &out, const Movie &m);//override the operator
     bool operator< (const Movie& other) const {//overrides the less than operator
-        return this->getReleaseYear() < other.getReleaseYear();
+        return this->getReleaseYear() < other.getReleaseYear();//oldest to most recent
     }
     struct CompareMoviesByDuration { //struct used to compare movies by duration using a functor
         bool operator () (const Movie& a, const Movie& b) const {
-            return a.getDuration() > b.getDuration();
+            return a.getDuration() > b.getDuration();//longest to shortest
         }
     };
 };
